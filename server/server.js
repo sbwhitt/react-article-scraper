611b47f81d97f468b5c78d9532e87d932ab84e8b
Samuel Whittenberger <sbw98978@gmail.com>
2023-06-29 19:58:56 -0400

    formatting

--- server/server.js
+++ server/server.js
@@ -7,38 +7,36 @@ const app = express();
 app.use(cors());
 
 app.get("/api/scrape", (req, res) => {
-    scrape.article(req.query.url)
-        .then(data => {
-            gpt("summarize this text in a few sentences: \n", data)
-                .then(gptRes => {
-                    res.send(gptRes.data.choices[0].message.content);
-                })
-                .catch(err => {
-                    console.log("gpt error");
-                    console.log(err);
-                    res.send(err);
-                });
-        })
-        .catch(err => {
-            console.log("scrape error");
-            console.log(err);
-            res.send(err);
-        });
+    scrape.article(req.query.url).then(data => {
+        gpt("summarize this text in a few sentences: \n", data)
+            .then(gptRes => {
+                res.send(gptRes.data.choices[0].message.content);
+            })
+            .catch(err => {
+                console.log("gpt error");
+                console.log(err);
+                res.send(err);
+            });
+    })
+    .catch(err => {
+        console.log("scrape error");
+        console.log(err);
+        res.send(err);
+    });
 });
 
 app.get("/api/prompt", (req, res) => {
-    scrape.text(req.query.url)
-        .then(data => {
-            gpt(req.query.prompt, data)
-                .then(gptRes => {
-                    res.send(gptRes.data.choices[0].message.content);
-                })
-                .catch(err => {
-                    console.log("gpt error");
-                    console.log(err);
-                    res.send(err);
-                });
-        });
+    scrape.text(req.query.url).then(data => {
+        gpt(req.query.prompt, data)
+            .then(gptRes => {
+                res.send(gptRes.data.choices[0].message.content);
+            })
+            .catch(err => {
+                console.log("gpt error");
+                console.log(err);
+                res.send(err);
+            });
+    });
 });
 
 const port = 4000;
